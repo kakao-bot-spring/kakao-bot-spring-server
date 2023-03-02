e68a9d353a2320d19609c9a2a4100d18a037644d
thuthi <whquddn55@gmail.com>
2023-03-02 15:52:08 +0900

    Initial commit

---
+++ src/main/java/com/thuthi/kakaobotspringserver/KakaoBotSpringServerApplication.java
@@ -0,0 +1,13 @@
+package com.thuthi.kakaobotspringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoBotSpringServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakaoBotSpringServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thuthi/kakaobotspringserver/KakaoBotSpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thuthi.kakaobotspringserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoBotSpringServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
