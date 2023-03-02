d796b254e1eb06f165ed05c9546e5f52425565eb
thuthi <whquddn55@gmail.com>
2023-03-02 16:21:44 +0900

    feat: 테스트용 api의 Method를 POST로 수정

--- src/main/java/com/thuthi/kakaobotspringserver/api/HelloApiController.java
+++ src/main/java/com/thuthi/kakaobotspringserver/api/HelloApiController.java
@@ -1,13 +1,13 @@
 package com.thuthi.kakaobotspringserver.api;
 
 import jakarta.annotation.Nullable;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloApiController {
-    @GetMapping("/")
+    @PostMapping("/")
     public String hello(@Nullable @RequestBody String request) {
         System.out.println("HelloApiController.hello");
         System.out.println("request = " + request);
